e7a6d1e4cee506353b24c25adb8e9ddfd3a29ad7
Kazionnikova <nikoletta.kazionnikova@refinitiv.com>
2022-02-24 00:10:17 +0200

    minor fixes

--- src/main/java/hw2/test/TestStreams.java
+++ src/main/java/hw2/test/TestStreams.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestStreams {
 
@@ -14,19 +15,16 @@ public class TestStreams {
     private final ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
     public TestStreams firstExercise(){
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(120,410,85,32,314,12));
         ArrayList<Integer> filtered = new ArrayList<>();
-        numbers.stream()
-                .filter(n -> n < 300)
-                .map(n -> n + 11)
+        Stream.of(120,410,85,32,314,12).limit(3)
+                .map(n -> n < 300 ? n + 11 : n)
                 .forEach(filtered::add);
         log.log(filtered + "\n");
         return this;
     }
 
     public TestStreams secondExercise(){
-        ArrayList<String> colors = new ArrayList<>(Arrays.asList("red", "green", "blue"));
-        Optional<String> first = colors.stream()
+        Optional<String> first = Stream.of("red", "green", "blue")
                 .sorted()
                 .findFirst();
         log.log(first.get() + "\n");
@@ -34,8 +32,7 @@ public class TestStreams {
     }
 
     public TestStreams thirdExercise(){
-        ArrayList<Integer> numbers3 = new ArrayList<>(Arrays.asList(3,4,1,40));
-        Optional<Integer> firstN = numbers3.stream()
+        Optional<Integer> firstN = Stream.of(3, 4, 1, 40)
                 .sorted()
                 .findFirst();
         log.log(firstN.get() + "\n");
@@ -43,9 +40,8 @@ public class TestStreams {
     }
 
     public TestStreams fourthExercise(){
-        ArrayList<String> fruits = new ArrayList<>(Arrays.asList("apple", "banana", "pear", "cherry", "pineapple"));
         ArrayList<String> filtered = new ArrayList<>();
-        fruits.stream()
+        Stream.of("apple", "banana", "pear", "cherry", "pineapple")
                 .filter((n) -> n.startsWith("p"))
                 .forEach(filtered::add);
         log.log(filtered + "\n");
@@ -53,20 +49,18 @@ public class TestStreams {
     }
 
     public TestStreams fifthExercise(){
-        ArrayList<String> words = new ArrayList<>(Arrays.asList("Java", "Rocks"));
         ArrayList<String> filtered = new ArrayList<>();
-        words.stream()
-                .map((n) -> n.toUpperCase(Locale.ROOT))
+        Stream.of("Java", "Rocks")
+                .map(String::toUpperCase)
                 .forEach(filtered::add);
         log.log(filtered + "\n");
         return this;
     }
 
     public TestStreams sixthExercise(){
-        ArrayList<Double> numbers6 = new ArrayList<>(Arrays.asList(1.5, 2.3, 3.7));
         ArrayList<Integer> filtered = new ArrayList<>();
-        numbers6.stream()
-                .map(Double::intValue)
+        Stream.of(1.5, 2.3, 3.7)
+                .mapToInt(Double::intValue)
                 .forEach(filtered::add);
         log.log(filtered + "\n");
         return this;
@@ -111,16 +105,14 @@ public class TestStreams {
     }
 
     public TestStreams eleventhExercise(){
-        ArrayList<Integer> numbers11 = new ArrayList<>(Arrays.asList(5,4,6,8,4,10,2,23,4));
         ArrayList<Integer> filtered = new ArrayList<>();
-        numbers11.stream().sorted().forEach(filtered::add);
+        Stream.of(5,4,6,8,4,10,2,23,4).sorted().forEach(filtered::add);
         log.log(filtered + "\n");
         return this;
     }
 
     public TestStreams twelfthExercise(){
-        ArrayList<Integer> numbers12 = new ArrayList<>(Arrays.asList(4,1,5,1,2,3,3,4,5,5));
-        ArrayList<Integer> filtered = numbers12.stream().sorted().distinct().collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Integer> filtered = Stream.of(4,1,5,1,2,3,3,4,5,5).sorted().distinct().collect(Collectors.toCollection(ArrayList::new));
         log.log(filtered + "\n");
         return this;
     }
